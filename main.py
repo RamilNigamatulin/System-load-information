27dd9903e0290b8b4a2de70f3fa03cf2ab66a148
ramil <i@rnigamatulin.ru>
2025-01-18 10:27:12 +0200

    Изменения: - код покрыт тестами; - оформлен README.md;

--- main.py
+++ main.py
@@ -20,23 +20,6 @@ def get_disk_usage():
     return f'{disk.free / (1024 ** 3):.2f} ГБ / {disk.total / (1024 ** 3):.2f} ГБ'
 
 
-# Функция для обновления меток
-def update_labels():
-    global recording
-    cpu_usage = get_cpu_usage()
-    memory_usage = get_memory_usage()
-    disk_usage = get_disk_usage()
-
-    label_2.config(text=f"Загруженность ЦП: {cpu_usage} %")
-    label_3.config(text=f"Загруженность ОЗУ: {memory_usage}")
-    label_4.config(text=f"Загруженность ПЗУ: {disk_usage}")
-
-    if recording:
-        save_to_database(cpu_usage, memory_usage, disk_usage)
-
-    root.after(500, update_labels)  # обновление каждую половину секунды
-
-
 # Функция для создания базы данных
 def create_database():
     conn = sqlite3.connect('system_usage.db')
@@ -66,30 +49,8 @@ def save_to_database(cpu_usage, memory_usage, disk_usage):
     conn.close()
 
 
-# Функция для обновления таймера
-def update_timer():
-    if recording:
-        elapsed_time = datetime.now() - start_time
-        timer_label.config(text=f"Прошло времени: {str(elapsed_time).split('.')[0]}")
-        root.after(1000, update_timer)
-
-
-# Функция для обработки нажатия на кнопку
-def click_button():
-    global recording, start_time
-    recording = not recording
-    if recording:
-        btn["text"] = "Остановить запись"
-        start_time = datetime.now()
-        timer_label.config(text="Прошло времени: 0:00:00")
-        update_timer()
-    else:
-        btn["text"] = "Начать запись"
-        timer_label.config(text="")
-
-
 # Функция для отображения статистики
-def statistics():
+def statistics():  # pragma: no cover
     stats_window = Toplevel(root)
     stats_window.title("Статистика")
     stats_window.geometry("600x400")
@@ -102,7 +63,7 @@ def statistics():
     stats_text = Text(stats_window, wrap=NONE)
     stats_text.pack(fill=BOTH, expand=True)
 
-    stats_text.delete(1.0, END)  # Очищаем текстовое поле
+    stats_text.delete(1.0, END)
     stats_text.insert(END, "Загрузка ЦП | Свободного ОЗУ | Свободного ПЗУ | Дата и время\n")
     stats_text.insert(END, "-" * 60 + "\n")
     for row in rows:
@@ -115,46 +76,86 @@ def statistics():
 def clear_statistics():
     conn = sqlite3.connect('system_usage.db')
     cursor = conn.cursor()
-    cursor.execute('DELETE FROM usage_data')  # Удаляем все записи из таблицы
+    cursor.execute('DELETE FROM usage_data')
     conn.commit()
     conn.close()
 
 
-# Основное окно
-root = Tk()
-root.title("Уровень загруженности ПК")
-root.geometry("1000x700")
-root.resizable(False, False)
+# Функция для обновления меток
+def update_labels():  # pragma: no cover
+    global recording
+    cpu_usage = get_cpu_usage()
+    memory_usage = get_memory_usage()
+    disk_usage = get_disk_usage()
 
-# Глобальные переменные
-recording = False  # Флаг для отслеживания состояния записи
-start_time = None  # Время начала записи
+    label_2.config(text=f"Загруженность ЦП: {cpu_usage} %")
+    label_3.config(text=f"Загруженность ОЗУ: {memory_usage}")
+    label_4.config(text=f"Загруженность ПЗУ: {disk_usage}")
 
-# Создаём базу данных и таблицу
-create_database()
+    if recording:  # pragma: no cover
+        save_to_database(cpu_usage, memory_usage, disk_usage)
 
-# Элементы интерфейса
-label_1 = Label(text="Уровень загруженности:", font=("Arial", 15))
-label_2 = Label(text=f"Загруженность ЦП: {get_cpu_usage()} %", font=("Arial", 13))
-label_3 = Label(text=f"Загруженность ОЗУ: {get_memory_usage()}", font=("Arial", 13))
-label_4 = Label(text=f"Загруженность ПЗУ: {get_disk_usage()}", font=("Arial", 13))
+    root.after(500, update_labels)
 
-timer_label = Label(text="", font=("Arial", 12))
-menu = ttk.Button(text="Статистика", command=statistics)
-clear_button = ttk.Button(text="Очистить статистику", command=clear_statistics)
-btn = ttk.Button(text="Начать запись", command=click_button)
 
-# Размещение элементов
-menu.place(relx=1.0, rely=0.0, anchor='ne', x=-100, y=10)
-clear_button.place(relx=1.0, rely=0.0, anchor='se', x=-100, y=100)
-label_1.pack(anchor='nw', padx=20, pady=40)
-label_2.pack(anchor='nw', padx=20)
-label_3.pack(anchor='nw', padx=20)
-label_4.pack(anchor='nw', padx=20)
-btn.pack(anchor='s', pady=50)
-timer_label.pack(anchor='s', pady=10)
+# Функция для обновления таймера
+def update_timer():  # pragma: no cover
+    if recording:
+        elapsed_time = datetime.now() - start_time
+        timer_label.config(text=f"Прошло времени: {str(elapsed_time).split('.')[0]}")
+        root.after(1000, update_timer)
 
-# Запуск обновления данных
-update_labels()
 
-root.mainloop()
+# Функция для обработки нажатия на кнопку
+def click_button():  # pragma: no cover
+    global recording, start_time
+    recording = not recording
+    if recording:
+        btn["text"] = "Остановить запись"
+        start_time = datetime.now()
+        timer_label.config(text="Прошло времени: 0:00:00")
+        update_timer()
+    else:
+        btn["text"] = "Начать запись"
+        timer_label.config(text="")
+
+
+# Функция для создания графического интерфейса
+def create_gui():  # pragma: no cover
+    global root, label_1, label_2, label_3, label_4, timer_label, menu, clear_button, btn, recording, start_time
+
+    root = Tk()
+    root.title("Уровень загруженности ПК")
+    root.geometry("1000x700")
+    root.resizable(False, False)
+
+    recording = False
+    start_time = None
+
+    create_database()
+
+    label_1 = Label(text="Уровень загруженности:", font=("Arial", 15))
+    label_2 = Label(text=f"Загруженность ЦП: {get_cpu_usage()} %", font=("Arial", 13))
+    label_3 = Label(text=f"Загруженность ОЗУ: {get_memory_usage()}", font=("Arial", 13))
+    label_4 = Label(text=f"Загруженность ПЗУ: {get_disk_usage()}", font=("Arial", 13))
+
+    timer_label = Label(text="", font=("Arial", 12))
+    menu = ttk.Button(text="Статистика", command=statistics)
+    clear_button = ttk.Button(text="Очистить статистику", command=clear_statistics)
+    btn = ttk.Button(text="Начать запись", command=click_button)
+
+    menu.place(relx=1.0, rely=0.0, anchor='ne', x=-100, y=10)
+    clear_button.place(relx=1.0, rely=0.0, anchor='se', x=-100, y=100)
+    label_1.pack(anchor='nw', padx=20, pady=40)
+    label_2.pack(anchor='nw', padx=20)
+    label_3.pack(anchor='nw', padx=20)
+    label_4.pack(anchor='nw', padx=20)
+    btn.pack(anchor='s', pady=50)
+    timer_label.pack(anchor='s', pady=10)
+
+    update_labels()
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    create_gui() # pragma: no cover
